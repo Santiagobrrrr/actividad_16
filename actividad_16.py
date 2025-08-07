3185a7e560c4f8fbb7f3d4cdcdd1a7bce70b2eca
Santiago <sjbatz@correo.url.edu.gt>
2025-08-07 09:59:43 -0600

    MENÚ

--- actividad_16.py
+++ actividad_16.py
@@ -6,7 +6,7 @@ class Book:
 
 list_books = []
 
-def add_books():
+def add_books(self):
     try:
         title = input("Ingrese el titulo del libro: ")
         author = input("Ingrese el autor del libro: ")
@@ -31,7 +31,36 @@ def remove_books():
         print("No hay libros registrados.\n")
     else:
         book_user = input("Ingrese el titulo de libro que desea eliminar: ").lower()
-        if book_user == book.title.lower():
-            list_books.remove(book_user)
-        else:
-            print("El libro no existe\n")
+        book_found = False
+        for book in list_books:
+            if book.title.lower() == book_user.lower():
+                list_books.remove(book_user)
+                book_found = True
+                break
+            else:
+                print(F"El libro {book.title} no existe\n")
+
+while True:
+    print(f"-- MENÚ BIBLIOTECA --")
+    print(f"1- Añadir libro")
+    print(f"2- Mostrar libros")
+    print(f"3- Eliminar libro")
+    print(f"4- Salir")
+
+    option_user = input(f"Ingrese la opción a la que desea ir: ")
+    match option_user:
+        case "1":
+            add_books()
+
+        case "2":
+            show_books()
+
+        case "3":
+            remove_books()
+
+        case "4":
+            print("Saliendo del programa...")
+            break
+
+        case _:
+            print(f"Valor inválido, intente de nuevo.")
